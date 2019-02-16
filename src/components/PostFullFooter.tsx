acf7aca4424df62a36b5043ca1186d4e8897d1ce
tehstormcrow <ibrahimayhankus@gmail.com>
2019-02-16 13:28:11 +0300

    style project page mobile paddings

--- src/components/PostFullFooter.tsx
+++ src/components/PostFullFooter.tsx
@@ -1,17 +1,9 @@
-import * as React from 'react';
 import styled from '@emotion/styled';
 
 const PostFullFoot = styled.footer`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 0 auto;
-  padding: 3vw 0 6vw 0;
-  max-width: 840px;
-
-  @media (max-width: 900px) {
-    margin: 0 20px;
-  }
 
   @media (max-width: 500px) {
     flex-direction: column;
@@ -20,8 +12,4 @@ const PostFullFoot = styled.footer`
   }
 `;
 
-const PostFullFooter: React.FunctionComponent = props => (
-  <PostFullFoot>{props.children}</PostFullFoot>
-);
-
-export default PostFullFooter;
+export default PostFullFoot;
